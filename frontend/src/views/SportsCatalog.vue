ca9a8032ee10565ea2e023130ecf7ee28acca635
Nel <neltharak.sgl@gmail.com>
2025-01-14 11:43:58 +0100

    components

--- frontend/src/views/SportsCatalog.vue
+++ frontend/src/views/SportsCatalog.vue
@@ -1,9 +1,23 @@
+
+<script setup>
+import FooterButtons from '/src/components/footer.vue'
+import HeaderBar from '/src/components/header.vue'
+import PageButtons from '/src/components/buttons.vue'
+</script>
+
 <template>
-    <h1> Hello world </h1>
-    
-    </template>
-    
-    <script setup>
-    
-    </script>
-    
+<div class="phone_container">
+   
+    <HeaderBar/>
+    <section class="presentation">
+        <h1>Catalogue des sports</h1>
+        <p>Trouvez le handisport qui vous va bien !
+        </p>
+    </section>
+
+    <PageButtons/>
+    <FooterButtons/>
+
+
+</div>
+</template>
