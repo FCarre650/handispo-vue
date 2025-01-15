d9bfa7a5a4dc168d8c3f5230e7a605449bdfb142
Maud Peleran <m.peleran@gmail.com>
2025-01-15 14:58:00 +0100

    filter for postal code ok

--- frontend/src/views/AssoList.vue
+++ frontend/src/views/AssoList.vue
@@ -2,10 +2,13 @@
     import FooterButtons from '/src/components/footer.vue'
     import HeaderBar from '/src/components/header.vue'
     import { associations } from "/src/use/useAssociation"
-    import {ref, onMounted} from 'vue'
+    import {ref, onMounted, computed} from 'vue'
 
     const associationList = ref([])
+    const formData = ref({})
 
+    const dialog = ref(false);
+    
     onMounted(async () => {
         const response = await fetch("/api/listAsso")
         associationList.value = await response.json() 
@@ -15,47 +18,131 @@
             associations.value[association.id] = association
     }
  })
+
+ const filteredList = computed(() => {
+        let searchValue = formData.value.postalCode
+        console.log(associationList.value.postalCode)
+        return associationList.value.filter(association => association.postalCode.includes(searchValue))
+    })
+
 </script>
 
 <template>
-    <div class="phone_container">
+    <div>
         <HeaderBar />
 
         <section class="AssoList">
             <h1>LISTE DES ASSOCIATIONS</h1>
-
         </section>
 
-        
+    <div class="bouton_haut">
+        <v-dialog
+        v-model="dialog"
+        >
+        <template v-slot:activator="{ props: activatorProps }">
+            <v-btn
+            class="btn_filtres"
+            text="FILTRES"
+            v-bind="activatorProps"
+            ></v-btn>
+        </template>
 
-        <section class="boutons">
-            <div class="bouton_bas">
-                <button class="btn_filtres">FILTRES</button>
-                <button class="btn_geolocalisation">GÉOLOCALISATION</button>
-            </div>
-            
-        </section>
+        <v-card
+            prepend-icon="mdi-account"
+            title="Filtres"
+        >
+            <v-card-text>
+            <v-row dense>
+                <v-col
+                cols="12"
+                md="4"
+                sm="6"
+                >
+                <v-text-field
+                    label="Recherche par code postal"
+                    v-model="formData.postalCode"
+                    required
+                ></v-text-field>
+                </v-col>
 
-        <section>
-            <div class="flex">
-                <v-list class="list">
-                    <v-list-item v-for="association of associationList" key="association.id"> 
-                        <div class=" boxAsso">
-                            <p>{{ association.nom }}</p>
-                            <p>{{ association.location }}</p>
-                            <p>{{ association.address }}</p>
-                            <p>{{ association.postalCode }} {{ association.city }}</p>
-                            
-                        </div>
-                    </v-list-item>
-                </v-list>
- 
-                <router-view class="detail"></router-view>
-            </div>
-        </section>
+                <v-col
+                cols="12"
+                md="4"
+                sm="6"
+                >
+                <v-select
+                    :items="['Badminton', 'Rugby']"
+                    label="Type de sport"
+                    required
+                ></v-select>
+                </v-col>
 
+                <v-col
+                cols="12"
+                sm="6"
+                >
+                <v-select
+                    :items="['Occitanie']"
+                    label="Région"
+                    required
+                ></v-select>
+                </v-col>
+
+                <v-col
+                cols="12"
+                sm="6"
+                >
+                <v-autocomplete
+                    :items="['Haute-Garonne', 'Lot', 'Ariège', 'Tarn', 'Aude']"
+                    label="Département"
+                    auto-select-first
+                    multiple
+                ></v-autocomplete>
+                </v-col>
+            </v-row>
+
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+                text="FERMER"
+                variant="plain"
+                @click="dialog = false"
+            ></v-btn>
+
+            <v-btn
+                color="primary"
+                text="Valider"
+                variant="tonal"
+                @click="dialog = false"
+            ></v-btn>
+            </v-card-actions>
+        </v-card>
+        </v-dialog>
+  </div>
+  
+    <section>
+        <div class="flex">
+            <v-list class="list">
+                <v-list-item v-for="association of filteredList" key="association.id"> 
+                    <div class=" boxAsso">
+                        <p>{{ association.nom }}</p>
+                        <p>{{ association.location }}</p>
+                        <p>{{ association.address }}</p>
+                        <p>{{ association.postalCode }} {{ association.city }}</p>
+                        
+                    </div>
+                </v-list-item>
+            </v-list>
+
+            <router-view class="detail"></router-view>
+        </div>
+    </section>
         <FooterButtons />
-
     </div>
 
 </template>
@@ -64,4 +151,19 @@
     .boxAsso{
         border:solid 1px;
     }
+
+    .btn_filtres {
+        text-align: center;
+        width: 100%; /* Prend 50% de la largeur du conteneur */
+        height: 300px;
+        padding: 20px; /* Espacement interne pour le bouton */
+        background-color: #d24b1a; /* Couleur de fond */
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 16px;
+        color: white; /* Couleur du texte */
+        border: none; /* Pas de bordure */
+        border-radius: 5px; /* Coins arrondis */
+        cursor: pointer;
+    }
+
 </style>
