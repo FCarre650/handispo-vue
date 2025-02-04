5afc719946e3973ef9421eb910afc80a2ed5999d
Fanny Carré <fannycarre650@gmail.com>
2025-02-04 11:30:04 +0100

    container size css

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -5,23 +5,23 @@ import HeaderBar from '/src/components/header.vue'
 </script>
 
 <template>
-    <div class="phone_container">
-        <HeaderBar/>
-    
-        <section class="carte">
-            <h1>ENORME CARTE!</h1>
-            
-        </section>
-    
-        <section class="boutons">
-            <div class="bouton_haut">
-                <button class="btn_catalogue">CAAAAAAAAAARTE</button>
-            </div>
-        </section>
-    
-        <FooterButtons/>
+   
+    <HeaderBar/>
 
-    </div>
+    <section class="carte">
+        <h1>ENORME CARTE!</h1>
+        
+    </section>
+
+    <section class="boutons">
+        <div class="bouton_haut">
+            <button class="btn_catalogue">CAAAAAAAAAARTE</button>
+        </div>
+    </section>
+
+    <FooterButtons/>
+
+  
     
 </template>
     
