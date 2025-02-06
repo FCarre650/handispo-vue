e2bcf00e9a8a82cf666490ab7f4140e7e42b8528
Maud Peleran <m.peleran@gmail.com>
2025-02-06 09:15:21 +0100

    button on map vue

--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -3,7 +3,11 @@
 
 <template>
     <header>
+        <a href="" >
         <img src="../assets/images/logo.png" alt="logo" class="logo">
+        </a>
+        <a href="" >
         <img src="../assets/images/account.png" alt="compte utilisateur" class="compte">
+        </a>
     </header>
 </template>
